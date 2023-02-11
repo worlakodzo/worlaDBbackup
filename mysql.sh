a655a88acbb3dd8cd58df47986dca78da76da5f0
Worlako Dzokoto <worlaco.dzo@gmail.com>
2023-02-11 10:47:04 +0000

    Feat: refactor mysql script

--- mysql.sh
+++ mysql.sh
@@ -10,17 +10,29 @@ destination=$6
 # Change to backup directory
 cd destination
 
+function run_backup(){
 
-timestamp=$(date +%Y-%m-%d_%H-%M-%S)
+    timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 
-# Backup the database
-mysqldump -h $db_host -u $db_user -p$db_password $database > $database"_backup_"$timestamp".sql"
+    # Backup the database
+    mysqldump -h $db_host -u $db_user -p$db_password $database > $database"_backup_"$timestamp".sql"
 
-# Zip the backup file
-zip $database"_backup_"$timestamp".zip" $database"_backup_"$timestamp".sql"
+    # Zip the backup file
+    zip $database"_backup_"$timestamp".zip" $database"_backup_"$timestamp".sql"
 
-# Remove the .sql file after zipping
-rm $database"_backup_"$timestamp".sql"
+    # Remove the .sql file after zipping
+    rm $database"_backup_"$timestamp".sql"
 
-# Find and delete backup files older than 30 days
-find . -name "$database"_backup_* -mtime +30 -exec rm {} \;
+}
+
+function remove_backups(){
+
+    # Find and delete backup files older than 30 days
+    find . -name "$database"_backup_* -mtime +30 -exec rm {} \;
+}
+
+
+
+# Backup
+run_backup
+remove_backups

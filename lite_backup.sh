4034e86525e70034441f327288383d155993b090
Worlako Dzokoto <worlaco.dzo@gmail.com>
2023-03-20 01:35:51 +0000

    Updated backup script

--- lite_backup.sh
+++ lite_backup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 destination="/var/www/backup_db/liteapp/" 
 
-       
-cd $destination
+
+cd "/var/www/backup_db/worlaDBbackup"
 bash mysql.sh $LITEAPP_DB_NAME $destination $LITEAPP_DB_HOST $LITEAPP_DB_USER $LITEAPP_DB_PASSWORD $LITEAPP_DB_PORT
--- roofdepot_backup.sh
+++ roofdepot_backup.sh
@@ -2,5 +2,5 @@
 destination="/var/www/backup_db/roofdepot/"
 
 
-cd $destination
+cd "/var/www/backup_db/worlaDBbackup"
 bash mysql.sh $RDL_APP_DB_NAME $destination $RDL_APP_DB_HOST $RDL_APP_DB_USER $RDL_APP_DB_PASSWORD $RDL_APP_DB_PORT
